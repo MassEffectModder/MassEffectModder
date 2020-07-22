17317abd0d7c35ee3275ae616c0c3ac4f19811e1
Paweł Kołodziejski <aquadran@users.sourceforge.net>
2020-07-22 07:13:27 +0200

    Added severzipwrapper replacement

--- MassEffectModder/Wrappers/Wrapper7Zip.cpp
+++ MassEffectModder/Wrappers/Wrapper7Zip.cpp
@@ -71,6 +71,41 @@ int LzmaCompress(unsigned char *src, unsigned int src_len,
     return status;
 }
 
+#ifdef _WIN32
+#define _WIN32_WINNT 0x0501
+#include <windows.h>
+BOOL WINAPI DllMain(HINSTANCE hin, DWORD reason, LPVOID lpvReserved) { return TRUE; }
+#define SEVENZIP_EXPORT __declspec(dllexport)
+#else
+#define SEVENZIP_EXPORT
+#endif
+
+extern "C" {
+SEVENZIP_EXPORT int SevenZipDecompress(unsigned char *src, unsigned int src_len, unsigned char *dst, unsigned int *dst_len)
+{
+    size_t len = *dst_len, sLen = src_len - LZMA_PROPS_SIZE;
+
+    int status = LzmaUncompress(dst, &len, &src[LZMA_PROPS_SIZE], &sLen, src, LZMA_PROPS_SIZE);
+    if (status == SZ_OK)
+        *dst_len = len;
+
+    return status;
+}
+
+SEVENZIP_EXPORT int SevenZipCompress(int compression_level, unsigned char *src, unsigned int src_len,
+                                     unsigned char *dst, unsigned int *dst_len)
+{
+    size_t len = *dst_len, propsSize = LZMA_PROPS_SIZE;
+    int status = LzmaCompressData(&dst[LZMA_PROPS_SIZE], &len, src, src_len, dst, &propsSize, compression_level,
+                               1 << 16, -1, -1, -1, -1, -1);
+    if (status == SZ_OK)
+        *dst_len = len + LZMA_PROPS_SIZE;
+
+    return status;
+}
+
+}
+
 extern "C" {
 #if defined(_WIN32)
 int sevenzip_unpack(const wchar_t *path, const wchar_t *output_path, int full_path);
