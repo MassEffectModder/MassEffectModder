d002fd34f2260bc4e13e9e3eda14941681b0ede5
Paweł Kołodziejski <aquadran@gmail.com>
2021-06-10 07:53:21 +0200

    update lods for LE

--- MassEffectModder/MassEffectModder/GameData/UserSettings.cpp
+++ MassEffectModder/MassEffectModder/GameData/UserSettings.cpp
@@ -27,37 +27,40 @@ void UserSettings::readLODIpc(MeType gameId, ConfigIni &engineConf)
 {
     if (gameId == MeType::ME1_TYPE)
     {
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_World=" + engineConf.Read("TEXTUREGROUP_World", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_WorldNormalMap=" + engineConf.Read("TEXTUREGROUP_WorldNormalMap", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_AmbientLightMap=" + engineConf.Read("TEXTUREGROUP_AmbientLightMap", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_LightAndShadowMap=" + engineConf.Read("TEXTUREGROUP_LightAndShadowMap", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_Environment_64=" + engineConf.Read("TEXTUREGROUP_Environment_64", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_Environment_128=" + engineConf.Read("TEXTUREGROUP_Environment_128", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_Environment_256=" + engineConf.Read("TEXTUREGROUP_Environment_256", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_Environment_512=" + engineConf.Read("TEXTUREGROUP_Environment_512", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_Environment_1024=" + engineConf.Read("TEXTUREGROUP_Environment_1024", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_VFX_64=" + engineConf.Read("TEXTUREGROUP_VFX_64", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_VFX_128=" + engineConf.Read("TEXTUREGROUP_VFX_128", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_VFX_256=" + engineConf.Read("TEXTUREGROUP_VFX_256", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_VFX_512" + engineConf.Read("TEXTUREGROUP_VFX_512", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_VFX_1024=" + engineConf.Read("TEXTUREGROUP_VFX_1024", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_APL_128=" + engineConf.Read("TEXTUREGROUP_APL_128", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_APL_256=" + engineConf.Read("TEXTUREGROUP_APL_256", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_APL_512=" + engineConf.Read("TEXTUREGROUP_APL_512", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_APL_1024=" + engineConf.Read("TEXTUREGROUP_APL_1024", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_GUI=" + engineConf.Read("TEXTUREGROUP_GUI", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_Promotional=" + engineConf.Read("TEXTUREGROUP_Promotional", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_Character_1024=" + engineConf.Read("TEXTUREGROUP_Character_1024", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_Character_Diff=" + engineConf.Read("TEXTUREGROUP_Character_Diff", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_Character_Norm=" + engineConf.Read("TEXTUREGROUP_Character_Norm", "TextureLODSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_Character_Spec=" + engineConf.Read("TEXTUREGROUP_Character_Spec", "TextureLODSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_World=" + engineConf.Read("TEXTUREGROUP_World", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_WorldSpecular=" + engineConf.Read("TEXTUREGROUP_WorldSpecular", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_WorldNormalMap=" + engineConf.Read("TEXTUREGROUP_WorldNormalMap", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_AmbientLightMap=" + engineConf.Read("TEXTUREGROUP_AmbientLightMap", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_ShadowMap=" + engineConf.Read("TEXTUREGROUP_ShadowMap", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_RenderTarget=" + engineConf.Read("TEXTUREGROUP_RenderTarget", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_Environment_64=" + engineConf.Read("TEXTUREGROUP_Environment_64", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_Environment_128=" + engineConf.Read("TEXTUREGROUP_Environment_128", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_Environment_256=" + engineConf.Read("TEXTUREGROUP_Environment_256", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_Environment_512=" + engineConf.Read("TEXTUREGROUP_Environment_512", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_Environment_1024=" + engineConf.Read("TEXTUREGROUP_Environment_1024", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_VFX_64=" + engineConf.Read("TEXTUREGROUP_VFX_64", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_VFX_128=" + engineConf.Read("TEXTUREGROUP_VFX_128", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_VFX_256=" + engineConf.Read("TEXTUREGROUP_VFX_256", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_VFX_512=" + engineConf.Read("TEXTUREGROUP_VFX_512", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_VFX_1024=" + engineConf.Read("TEXTUREGROUP_VFX_1024", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_APL_128=" + engineConf.Read("TEXTUREGROUP_APL_128", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_APL_256=" + engineConf.Read("TEXTUREGROUP_APL_256", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_APL_512=" + engineConf.Read("TEXTUREGROUP_APL_512", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_APL_1024=" + engineConf.Read("TEXTUREGROUP_APL_1024", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_UI=" + engineConf.Read("TEXTUREGROUP_UI", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_Promotional=" + engineConf.Read("TEXTUREGROUP_Promotional", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_Character_1024=" + engineConf.Read("TEXTUREGROUP_Character_1024", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_Character_Diff=" + engineConf.Read("TEXTUREGROUP_Character_Diff", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_Character_Norm=" + engineConf.Read("TEXTUREGROUP_Character_Norm", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_Character_Spec=" + engineConf.Read("TEXTUREGROUP_Character_Spec", "SystemSettings"));
     }
     else if (gameId == MeType::ME2_TYPE)
     {
         ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_World=" + engineConf.Read("TEXTUREGROUP_World", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_WorldSpecular=" + engineConf.Read("TEXTUREGROUP_WorldSpecular", "SystemSettings"));
         ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_WorldNormalMap=" + engineConf.Read("TEXTUREGROUP_WorldNormalMap", "SystemSettings"));
         ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_AmbientLightMap=" + engineConf.Read("TEXTUREGROUP_AmbientLightMap", "SystemSettings"));
-        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_LightAndShadowMap=" + engineConf.Read("TEXTUREGROUP_LightAndShadowMap", "SystemSettings"));
+        ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_ShadowMap=" + engineConf.Read("TEXTUREGROUP_ShadowMap", "SystemSettings"));
         ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_RenderTarget=" + engineConf.Read("TEXTUREGROUP_RenderTarget", "SystemSettings"));
         ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_Environment_64=" + engineConf.Read("TEXTUREGROUP_Environment_64", "SystemSettings"));
         ConsoleWrite(QString("[IPC]LODLINE ") + "TEXTUREGROUP_Environment_128=" + engineConf.Read("TEXTUREGROUP_Environment_128", "SystemSettings"));
